f79bd258e4218e789fe2be65836acc62d3204b9a
francisconascimento <francisconascimento@users.noreply.github.com>
2020-04-22 15:50:59 +0100

    intermediate save

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 sudo apt update
-sudo apt install software-properties-common
+sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible curl git hub
+sudo apt -y install ansible git hub
+git config --global hub.protocol https
+hub clone workstation-builder
+cd workstation-builder
