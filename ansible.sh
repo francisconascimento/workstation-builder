052827363e6df99cbb8a672b55c95bf4170dadb6
Francisco Nascimento <github@mazehill.co.uk>
2020-04-24 10:10:46 +0100

    ansible.sh content to start.sh
    Moved (most of) ansible.sh content to start.sh.

--- ansible.sh
+++ ansible.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-sudo apt update
-sudo apt -y install software-properties-common
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt -y install ansible git hub python-apt
-git config --global hub.protocol https
-hub clone workstation-builder
-cd workstation-builder
 ansible-playbook -i hosts --ask-become-pass playbook.yml
