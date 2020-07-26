ce6cd509c89bbbff012d58a03173d088df7a76f0
francisconascimento <francisconascimento@users.noreply.github.com>
2020-07-26 13:05:06 +0100

    Update start.sh
    Added apt-transport-https, removed git

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt -y install ansible git hub python-apt
+sudo apt -y install ansible apt-transport-https hub python-apt
 git config --global hub.protocol https
 hub clone workstation-builder
 cd workstation-builder
