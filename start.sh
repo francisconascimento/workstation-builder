90c05a17c6e6491b11b946a568812f4bbe48eeb3
francisconascimento <francisconascimento@users.noreply.github.com>
2020-07-26 13:06:20 +0100

    Update start.sh
    added 'cd ~'

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ sudo apt -y install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt -y install ansible apt-transport-https hub python-apt
 git config --global hub.protocol https
+cd ~
 hub clone workstation-builder
 cd workstation-builder
 chmod 755 *.sh
